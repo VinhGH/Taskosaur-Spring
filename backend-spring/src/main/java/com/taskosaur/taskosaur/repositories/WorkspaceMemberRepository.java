33d52ef715601c09f8d7602834a420325e7885de
VinhGH <github.for.vinh@gmail.com>
2026-08-20 10:57:08 +0700

    feat: DTO and repositories

---
+++ backend-spring/src/main/java/com/taskosaur/taskosaur/repositories/WorkspaceMemberRepository.java
@@ -0,0 +1,16 @@
+package com.taskosaur.taskosaur.repositories;
+
+
+import com.taskosaur.taskosaur.models.WorkspaceMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface WorkspaceMemberRepository extends JpaRepository<WorkspaceMember, String> {
+    List<WorkspaceMember> findByWorkspaceId(String workspaceId);
+    Optional<WorkspaceMember> findByWorkspaceIdAndUserId(String workspaceId, String userId);
+    boolean existsByWorkspaceIdAndUserId(String workspaceId, String userId);
+
+}
