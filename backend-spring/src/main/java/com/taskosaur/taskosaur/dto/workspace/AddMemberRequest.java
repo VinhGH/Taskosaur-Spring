9af123c3f88e23592c7b839cb21ac6544bf53944
VinhGH <github.for.vinh@gmail.com>
2026-08-20 13:00:41 +0700

    to chuc package dto

--- backend-spring/src/main/java/com/taskosaur/taskosaur/dto/workspace/AddMemberRequest.java
+++ backend-spring/src/main/java/com/taskosaur/taskosaur/dto/workspace/AddMemberRequest.java
@@ -1,4 +1,24 @@
 package com.taskosaur.taskosaur.dto.workspace;
 
+import com.taskosaur.taskosaur.enums.WorkspaceRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AddMemberRequest {
+    @NotBlank(message = " user ID không được để trống")
+    private String userId;
+
+
+    @NotBlank(message = " workspace Id không được để trống")
+    private String workspaceId;
+
+    @NotNull(message = "Role không được để trống")
+    private WorkspaceRole role;
 }
